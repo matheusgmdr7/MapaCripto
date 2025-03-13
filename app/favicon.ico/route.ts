4f0115f8a49d650ab98f0ef441d64cf8e17de504
matheusgmdr7 <matheusgmdr7@gmail.com>
2025-03-13 01:19:56 -0300

    Update route.ts

--- app/favicon.ico/route.ts
+++ app/favicon.ico/route.ts
@@ -4,7 +4,7 @@ import type { NextRequest } from "next/server"
 export async function GET(request: NextRequest) {
   return new ImageResponse(
     <div
-      style={{
+     <style={{
         fontSize: 32,
         background: "black",
         width: "100%",
