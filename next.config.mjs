75839be5f46e948f10ca3acd9ecd045b54dde75d
matheusgmdr7 <matheusgmdr7@gmail.com>
2025-03-13 01:25:13 -0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,6 @@
-let userConfig = undefined
-try {
-  userConfig = await import('./v0-user-next.config')
-} catch (e) {
-  // ignore error
-}
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -16,33 +10,11 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  experimental: {
-    webpackBuildWorker: true,
-    parallelServerBuildTraces: true,
-    parallelServerCompiles: true,
-  },
-}
+  // Desabilita a geração de arquivos .gz
+  compress: false,
+  // Importante: como estamos usando exportação estática, não podemos usar Server Components
+  // ou Server Actions. Tudo deve ser renderizado no cliente.
+};
 
-mergeConfig(nextConfig, userConfig)
+export default nextConfig;
 
-function mergeConfig(nextConfig, userConfig) {
-  if (!userConfig) {
-    return
-  }
-
-  for (const key in userConfig) {
-    if (
-      typeof nextConfig[key] === 'object' &&
-      !Array.isArray(nextConfig[key])
-    ) {
-      nextConfig[key] = {
-        ...nextConfig[key],
-        ...userConfig[key],
-      }
-    } else {
-      nextConfig[key] = userConfig[key]
-    }
-  }
-}
-
-export default nextConfig
