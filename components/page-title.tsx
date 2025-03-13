67617980365478e166d4c398eb88f6e74d7b3629
matheusgmdr7 <matheusgmdr7@gmail.com>
2025-03-13 01:33:14 -0300

    Update page-title.tsx

--- components/page-title.tsx
+++ components/page-title.tsx
@@ -10,7 +10,7 @@ export default function PageTitle({ title = "MapaCripto" }: PageTitleProps) {
   useEffect(() => {
     // Atualiza o título da página
     document.title = title
-
+    
     // Limpa ao desmontar
     return () => {
       document.title = "MapaCripto"
